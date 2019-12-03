b85d042299988211348c89b6afcb96b94665cd69
Joshua Browne <joshua.browne@s.elim.school.nz>
2019-12-03 21:25:50 +1300

    Camera Movement
    - Camera moves with crosshair in centre on mouse movement
    - Scroll whell causes camera zoom
    - Zoom shrinks crosshair

---
+++ Unity/WatcherUnity/Assets/PGM.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PGM : MonoBehaviour
+{
+    public static PGM Instance { get; private set; }
+
+    public float FOV;
+
+    private void Awake()
+    {
+        if (Instance != null)
+            Destroy(gameObject);
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Unity/WatcherUnity/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [Header("Mouse Adjust Speed")]
+    [SerializeField]
+    private float SpeedH, SpeedV, SpeedS;
+    [Header("Defualt FOV")]
+    [SerializeField]
+    [Range(20f, 55f)]
+    private float dFOV;
+
+    private float yaw, pitch, scroll;
+
+    Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.fieldOfView = dFOV;
+        scroll = dFOV;
+        //Stop mouse from moving as well as hides it
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+
+        //Add adjustments to angles
+        yaw += SpeedH * Input.GetAxis("Mouse X");
+        pitch -= SpeedV * Input.GetAxis("Mouse Y");
+
+
+        //Change camera direction
+        transform.eulerAngles = new Vector3(pitch, yaw, 0);
+
+        scroll += SpeedS * Input.GetAxis("Mouse ScrollWheel");
+        if (scroll > 55)
+            scroll = 55;
+        if(scroll < 20)
+            scroll = 20;
+
+        cam.fieldOfView = scroll;
+        PGM.Instance.FOV = scroll;
+
+        //<<Finds what the camera is looking at>>
+        //Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        //RaycastHit hit;
+        //if (Physics.Raycast(ray, out hit))
+        //    print("I'm looking at " + hit.transform.name);
+        //else
+        //print("I'm looking at nothing!");
+    }
+}
