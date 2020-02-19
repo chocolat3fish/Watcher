292fadc6e1de44b18bd4f1b1db8d702ab83759ac
Aidan <aidan@Aidans-MacBook-Air.local>
2020-02-20 11:56:14 +1300

    Spinny chair, Office model, small lighting edits
    Added an office chair
    - Spins around with the player's camera
    
    Added an office object
    - Better than using a bunch of planes
    
    Lighting edited
    - Changed lighting from gamma to linear in project settings, edits to the lights in the game

---
+++ Unity/WatcherUnity/Assets/Scripts/ChairSpin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairSpin : MonoBehaviour
+{
+
+    public Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = Quaternion.Euler(0, mainCamera.transform.eulerAngles.y, 0);
+    }
+}
